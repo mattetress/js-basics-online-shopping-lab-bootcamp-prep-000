8a634e65b9ec8c184e6cdda89c69e50cc7e408b1
Matthew Etress <mattetress@me.com>
2018-08-28 23:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   } else {
     let list = []
-    for (let i = 0; i < cart.length; i++) {
+    for (let i = 0; i < cart.length - 1; i++) {
       list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
 
     }
