6f4ae0279cd633c9ee43fe547b9b80cf755ad07b
Matthew Etress <mattetress@me.com>
2018-08-28 23:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
-    return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   } else {
     let list = []
     for (let i = 0; i < cart.length; i++) {
