2c1a402a2d5a3d2cba7c4eaee336d0dbc64df03e
Matthew Etress <mattetress@me.com>
2018-08-28 23:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,17 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
-  }
-  for (let i = 0; i < cart.length; i++) {
-    let list = [];
-    let output = [];
-    if (cart.length === 1) {
-      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`;
-    } else if (cart.length === 2) {
-      output = `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
-    } else {
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  } else {
+    for (let i = 0; i < cart.length; i++) {
+      let list = [];
       list.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
-      output = `In your cart, you have ${list.join(', ')}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
-    } return output;
+
+    }
+    return `In your cart, you have ${list.join(', ')}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
   }
 }
 
