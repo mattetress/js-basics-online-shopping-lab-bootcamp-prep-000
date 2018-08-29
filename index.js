10e2a390bbc1655986cc8ca595c83f57257a8729
Matthew Etress <mattetress@me.com>
2018-08-29 00:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ function placeOrder(cardNumber) {
   } else {
     
     cart.length = 0;
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
