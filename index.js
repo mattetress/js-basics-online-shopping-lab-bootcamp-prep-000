ff38f4825321d1030cf28945d7f2909bdffb2d12
Matthew Etress <mattetress@me.com>
2018-08-29 00:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === "") {
+  if (cardNumber === null) {
     return "Sorry, we don't have a credit card on file for you.";
   } 
 }
