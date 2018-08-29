179e98d1e3421306d51352586df8efdd2814273d
Matthew Etress <mattetress@me.com>
2018-08-29 00:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  } cart.length = 0;
+    let totalOut = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   cart.length = 0;
+   return totalOut;
+  }
 }
